eba0d3bee17985e00e24df913e5dbf36a37b7722
Chandrakant Singh <aryalinux11@gmail.com>
2023-05-02 08:20:50 +0530

    Updated code for linux headers

--- base-system/cross-toolchain/003-linux-headers.sh
+++ base-system/cross-toolchain/003-linux-headers.sh
@@ -13,6 +13,12 @@ if ! grep "$NAME" /sources/build-log; then
 
 cd /sources
 
+TARBALL=linux-6.2.11.tar.xz
+DIRECTORY=$(tar tf $TARBALL | cut -d/ -f1 | uniq)
+
+tar xf $TARBALL
+cd $DIRECTORY
+
 
 make mrproper
 make headers
