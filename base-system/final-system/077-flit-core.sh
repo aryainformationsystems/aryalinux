554ba0a61faf2b1a7294d47807fe5fc01e2e0768
Chandrakant Singh <chandrakantsingh@protonmail.com>
2023-05-03 14:14:18 +0530

    Fixed flit

--- base-system/final-system/077-flit-core.sh
+++ base-system/final-system/077-flit-core.sh
@@ -13,6 +13,11 @@ if ! grep "$NAME" /sources/build-log; then
 
 cd /sources
 
+TARBALL=flit_core-3.8.0.tar.gz
+DIRECTORY=$(tar tf $TARBALL | cut -d/ -f1 | uniq)
+
+tar xf $TARBALL
+cd $DIRECTORY
 
 pip3 wheel -w dist --no-build-isolation --no-deps $PWD
 pip3 install --no-index --no-user --find-links dist flit_core
