c06d86a1ad771eaf9d4f3d00c67c7ca401f66b09
Chandrakant Singh <chandrakantsingh@protonmail.com>
2023-05-12 15:47:48 +0530

    Fixed mesa script

--- applications/mesa.sh
+++ applications/mesa.sh
@@ -61,32 +61,15 @@ export XORG_PREFIX=/usr
 mkdir build &&
 cd    build &&
 
-meson --prefix=$XORG_PREFIX          \
-      --sysconfdir=/etc              \
-      -Dllvm=true                    \
-      -Dshared-llvm=true             \
-      -Degl=true                     \
-      -Dshared-glapi=true            \
-      -Dgallium-xa=true              \
-      -Dgallium-nine=true            \
-      -Dgallium-vdpau=true           \
-      -Dgallium-va=true              \
-      -Ddri3=true                    \
-      -Dglx=dri                      \
-      -Dosmesa=true                  \
-      -Dgbm=true                     \
-      -Dglx-direct=true              \
-      -Dgles1=true                   \
-      -Dgles2=true                   \
-      -Dvalgrind=false               \
-      -Ddri-drivers=auto             \
-      -Dgallium-drivers=auto         \
-      -Dplatforms=auto               \
-      -Dvulkan-drivers=auto          \
-      ..                             &&
-
-unset GALLIUM_DRIVERS DRI_DRIVERS EGL_PLATFORMS &&
-
+meson setup                   \
+      --prefix=$XORG_PREFIX   \
+      --buildtype=release     \
+      -Dplatforms=x11,wayland \
+      -Dgallium-drivers=auto  \
+      -Dvulkan-drivers=auto   \
+      -Dvalgrind=disabled     \
+      -Dlibunwind=disabled    \
+      ..                      &&
 ninja
 
 sudo rm -rf /tmp/rootscript.sh
