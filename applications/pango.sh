f689a78d7c3a8dc6a443de09737a2dfdd3df5a31
Chandrakant Singh <chandrakantsingh@protonmail.com>
2023-05-13 06:40:06 +0530

    Removed doc builder commands

--- applications/pango.sh
+++ applications/pango.sh
@@ -60,9 +60,7 @@ meson setup --prefix=/usr          \
             --wrap-mode=nofallback \
             ..                     &&
 ninja
-sed "/docs_dir =/s@\$@ / 'pango-1.50.14'@" -i ../docs/meson.build &&
-meson configure -Dgtk_doc=true                                    &&
-ninja
+
 sudo rm -rf /tmp/rootscript.sh
 cat > /tmp/rootscript.sh <<"ENDOFROOTSCRIPT"
 ninja install
